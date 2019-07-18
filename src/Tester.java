2312e0cc71e9ff17658f53485d450a862090a53f
John Patrick Gonzales <gonzaless424@gmail.com>
2019-07-18 18:53:22 +0800

    Delete Tester.java

--- src/Tester.java
+++
@@ -1,9 +0,0 @@
-import controller.employee.EmployeeController;
-
-public class Tester {
-
-	public static void main(String[] args) {
-		EmployeeController empController = new EmployeeController();
-		empController.init();
-	}
-}
